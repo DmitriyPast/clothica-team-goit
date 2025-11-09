a8d67eca4bfa0fb8b5919cd884d9ec2ff975eef5
your_name <pastushenkodmitry2003@gmail.com>
2025-11-09 10:49:28 +0200

    add all GET requests to serverApi for prefetching

--- lib/api/serverApi.ts
+++ lib/api/serverApi.ts
@@ -2,6 +2,9 @@ import type { User } from '@/types/user';
 import internalApi from './api';
 import { cookies } from 'next/headers';
 import { Gender, Good, Size } from '@/types/good';
+import { Order } from '@/types/order';
+import { Feedback } from '@/types/feedback';
+import { Category } from '@/types/category';
 
 export interface FetchGoodsResponse {
   goods: Good[];
@@ -54,4 +57,64 @@ export async function fetchGoodsServer(
 export async function fetchGoodById(goodId: string): Promise<Good> {
   const { data } = await internalApi.get<Good>(`/goods/${goodId}`);
   return data;
+}
+
+export interface FetchOrdersResponse {
+  orders: Order[];
+}
+
+//GET fetch all orders
+export async function fetchAllOrders(): Promise<FetchOrdersResponse> {
+  return (await internalApi.get<FetchOrdersResponse>('orders')).data;
+}
+
+//GET fetch order by id
+export async function fetchOrderById(orderId: string) {
+  return (await internalApi.get<Order>(`orders/${orderId}`)).data;
+}
+
+export interface FetchCategoriesParams {
+  page?: number;
+  perPage?: number;
+}
+
+export interface FetchCategoriesResponse {
+  page: number;
+  perPage: number;
+  totalCategories: number;
+  totalPages: number;
+  catedories: Category[];
+}
+
+//GET categories
+export async function fetchCategories(
+  params: FetchCategoriesParams
+): Promise<FetchCategoriesResponse> {
+  return (
+    await internalApi.get<FetchCategoriesResponse>('categories', { params })
+  ).data;
+}
+
+export interface FetchFeedbacksParams {
+  productId: string;
+  page?: number;
+  perPage?: number;
+}
+
+export interface FetchFeedbacksResponse {
+  feedbacks: Feedback[];
+  productId: string;
+  totalFeedbacks: number;
+  page: number;
+  perPage: number;
+  totalPages: number;
+}
+
+//GET feedbacks
+export async function fetchFeedbacks(
+  params: FetchFeedbacksParams
+): Promise<FetchFeedbacksParams> {
+  return (
+    await internalApi.get<FetchFeedbacksResponse>('feedbacks', { params })
+  ).data;
 }
