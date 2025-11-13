0683a5759d5300435c32383bfcc0b37e6d30eecd
your_name <pastushenkodmitry2003@gmail.com>
2025-11-13 17:28:15 +0200

    test MessageNoInfo

--- app/(public routes)/page.tsx
+++ app/(public routes)/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import MessageNoInfo from '@/components/MessageNoInfo/MessageNoInfo';
 import Hero from '../../components/Hero/Hero';
 import Style from '../../components/Style/Style';
 
@@ -6,6 +9,27 @@ export default function HomePage() {
     <div className="container">
       <Hero />
       <Style />
+      <MessageNoInfo
+        text="Driving in my car right after a beer
+         Hey, that bump is shaped like a deer 
+         DUI? 
+         How about you die? 
+         I'll go a hundred miles An hour! 
+         Little do you know, I filled up on gas 
+         Imma get your fountain-making ass 
+         Pulverize this fuck 
+         ​With my Bergentrück
+         It seems you're out of luck!"
+        buttonText="TRUCK!"
+        onClick={handleOpenNewTab}
+      />
     </div>
   );
 }
+const handleOpenNewTab = () => {
+  window.open(
+    'https://www.youtube.com/watch?v=E1epC9YEkrs',
+    '_blank',
+    'noopener,noreferrer'
+  );
+};
