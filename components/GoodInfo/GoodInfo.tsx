2bea4891df3b17dc79a5bbbcc7441627c76115a5
your_name <pastushenkodmitry2003@gmail.com>
2025-11-16 00:11:46 +0200

    barebones positioning and styles

--- components/GoodInfo/GoodInfo.tsx
+++ components/GoodInfo/GoodInfo.tsx
@@ -1,18 +1,36 @@
 'use client';
 
-// import css from './GoodInfo.module.css';
+import Link from 'next/link';
+import css from './GoodInfo.module.css';
 import { Good } from '@/types/good';
 
 type Props = { good: Good };
 
 export default function GoodInfo({ good }: Props) {
   return (
-    <div >
-      <img src={good.image} alt={good.name}  />
+    <div>
+      <img src={good.image} alt={good.name} className={css.goodImg} />
       <h3>{good.name}</h3>
-      <p>Категорія: {good.category?.name}</p>
-      <p>Ціна: {good.price?.value}{good.price?.currency} ₴</p>
-      <button>Детальніше</button>
+      {/* <p>Категорія: {good.category?.name}</p> */}
+      <div className="stats">
+        <svg height={24} width={24}>
+          <use href="/sprite.svg#star-filled"></use>
+        </svg>
+        <span>0</span>
+        <svg height={24} width={24}>
+          <use href="/sprite.svg#comment"></use>
+        </svg>
+        <span>0</span>
+      </div>
+      <p>
+        Ціна: {good.price?.value}
+        {good.price?.currency} ₴
+      </p>
+      <Link
+        href={`/goods/${good._id}`}
+        className={`btn btn-secondary ${css.btnDetails}`}>
+        Детальніше
+      </Link>
     </div>
   );
 }
