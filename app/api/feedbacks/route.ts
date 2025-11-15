94df5565322905162b8725e715b7a97aa5f23968
your_name <pastushenkodmitry2003@gmail.com>
2025-11-15 13:16:30 +0200

    app/api/feedbacks fixed, added ReviewsList to the main page

--- app/api/feedbacks/route.ts
+++ app/api/feedbacks/route.ts
@@ -1,19 +1,32 @@
-import { NextResponse } from "next/server";
-import { fetchFeedbacks } from "@/lib/api/serverApi";
+import { NextResponse } from 'next/server';
+// import { fetchFeedbacks } from '@/lib/api/serverApi';
+import { api } from '../api';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const productId = searchParams.get("productId") || "";
-  const page = Number(searchParams.get("page")) || 1;
-  const perPage = Number(searchParams.get("perPage")) || 10;
+  const productId = searchParams.get('productId') || '';
+  const page = Number(searchParams.get('page')) || 1;
+  const perPage = Number(searchParams.get('perPage')) || 10;
+
+  // try {
+  //   const data = await fetchFeedbacks({ productId, page, perPage });
+  //   return NextResponse.json(data);
+  // } catch (error) {
+  //   console.error("Помилка при отриманні відгуків:", error);
+  //   return NextResponse.json(
+  //     { message: "Не вдалося отримати відгуки" },
+  //     { status: 500 }
+  //   );
+  // }
 
   try {
-    const data = await fetchFeedbacks({ productId, page, perPage });
+    const data = (await api.get('/feedbacks')).data;
+    // console.log(data);
     return NextResponse.json(data);
   } catch (error) {
-    console.error("Помилка при отриманні відгуків:", error);
+    console.error('Помилка при отриманні відгуків:', error);
     return NextResponse.json(
-      { message: "Не вдалося отримати відгуки" },
+      { message: 'Не вдалося отримати відгуки' },
       { status: 500 }
     );
   }
