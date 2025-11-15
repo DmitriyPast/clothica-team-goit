7f0bffeda2687f2bb48abd4984848e28930750db
your_name <pastushenkodmitry2003@gmail.com>
2025-11-15 15:20:59 +0200

    .

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -4,7 +4,7 @@ export type ApiError<T = { message?: string; error?: string }> = AxiosError<T>;
 const env = process.env.NEXT_PUBLIC_API_URL;
 let baseURL = env ? env + '/api' : 'http://localhost:3000/api';
 const isProd = process.env.NODE_ENV === 'production';
-if (isProd) baseURL = 'https//clothica-team-goit-p9hv.vercel.app/api'; //Vercel is stupid
+if (isProd) baseURL = 'https://clothica-team-goit-p9hv.vercel.app/api'; //Vercel is stupid
 
 const internalApi = axios.create({
   baseURL,
