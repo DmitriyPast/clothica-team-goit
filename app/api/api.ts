1f6ad6f060a59fb0429c42c86a08c624c54e923d
Oleksandr Maliuk <maliukolexandr@gmail.com>
2025-11-20 19:57:18 +0200

    path edit

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL:'http://localhost:3030/api',
-  // baseURL: 'https://clothica-api-team-work.onrender.com/api',
+  // baseURL:'http://localhost:3030/api',
+  baseURL: 'https://clothica-api-team-work.onrender.com/api',
   withCredentials: true,
 });
